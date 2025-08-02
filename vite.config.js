023f156ebe00f856d49dac7564f5863ab8cc738d
Alberto Ruiz <alruizmail@gmail.com>
2025-08-01 22:24:39 -0600

    Build test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,27 @@ export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
   build: {
-    assetsDir: 'assets'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+          icons: ['@fortawesome/fontawesome-svg-core', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons', '@fortawesome/free-solid-svg-icons', '@fortawesome/react-fontawesome']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  },
+  preview: {
+    port: 8080,
+    host: true
   }
 })
